d58ee318213a42c4cf131e1756c32133ed13f8cc
Kildyt <kildyt@gmail.com>
2018-09-01 16:22:48 +0200

    Added linter

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,23 +4,23 @@ const fs = require('fs')
 const { parse } = require('../')
 
 const createFileTest = (name, xhtml, expected) => {
-	test(`${name} - "${xhtml}"`, () => {
-		const actual = []
-		parse(xhtml, {
-			opentag(name) { actual.push({ opentag: name }) },
-			closetag(name) { actual.push({ closetag: name }) },
-			attribute(name, value) { actual.push({ attribute: [name, value] }) },
-			text(text) { actual.push({ text }) },
-			comment(comment) { actual.push({ comment }) },
-			instruction(instruction) { actual.push({ instruction }) },
-		})
-		expect(actual).toEqual(expected)
-	})
+  test(`${name} - "${xhtml}"`, () => {
+    const actual = []
+    parse(xhtml, {
+      opentag (name) { actual.push({ opentag: name }) },
+      closetag (name) { actual.push({ closetag: name }) },
+      attribute (name, value) { actual.push({ attribute: [name, value] }) },
+      text (text) { actual.push({ text }) },
+      comment (comment) { actual.push({ comment }) },
+      instruction (instruction) { actual.push({ instruction }) }
+    })
+    expect(actual).toEqual(expected)
+  })
 }
 
 const paths = glob.sync('./__tests__/**/*.yml')
 for (const path of paths) {
-	const file = fs.readFileSync(path, 'utf-8')
-	const { name, xhtml, expected } = yaml.safeLoad(file)
-	createFileTest(name, xhtml, expected)
+  const file = fs.readFileSync(path, 'utf-8')
+  const { name, xhtml, expected } = yaml.safeLoad(file)
+  createFileTest(name, xhtml, expected)
 }
